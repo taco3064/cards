be707e4400bf16ef2c91c66d10f9633b2178e4e1
LAPTOP-1DQ6UC5L\Taco <tabacotaco@gmail.com>
2025-11-18 01:57:53 +0800

    style(deck): update styles

--- src/containers/Deck/styled.ts
+++ src/containers/Deck/styled.ts
@@ -3,8 +3,10 @@ import type { CardDeckProps } from './types';
 
 export default {
   Container: styled.section`
+    position: relative;
     display: flex;
     flex-direction: column;
+    flex-wrap: nowrap;
     align-items: center;
     width: calc(100% - 48px);
     height: 100%;
@@ -14,18 +16,17 @@ export default {
   `,
   CardDeck: styled.div<CardDeckProps>`
     position: relative;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+    top: -10%;
     width: 100%;
-    padding-top: calc(100dvh / 5);
-    min-height: ${({ $height }) => `${$height * 2}px`};
+    height: 100%;
     transform-style: preserve-3d;
     transform: rotate3d(1, 0.2, -0.5, 45deg);
+    transform-origin: center;
 
-    ${({ $cardClassName, $width }) => `
+    ${({ $cardClassName, $width, $height }) => `
       & > .${$cardClassName} {
         position: absolute;
+        top: calc(50% - ${$height / 2}px);
         left: calc(50% - ${$width / 2}px);
       }
     `}
