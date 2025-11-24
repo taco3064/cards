2590e1b943db63da4ff6be9c1734167b21c9e5eb
LAPTOP-1DQ6UC5L\Taco <tabacotaco@gmail.com>
2025-11-24 13:59:40 +0800

    build(vite.config.ts): update vite config for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite';
 
+import { name } from './package.json';
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV !== 'production' ? '/' : `/${name}`,
   server: {
     open: true,
   },
